41a3ddc6c51b6ef0e4d09adc46a170b7973c5eb8
Pavel Iliev <2198356+pavele@users.noreply.github.com>
2025-05-04 00:08:37 +0300

    Update formula to version 1.0.7

--- Formula/home-server.rb
+++ Formula/home-server.rb
@@ -1,10 +1,10 @@
 class HomeServer < Formula
   desc "Local nginx reverse-proxy stack under ~/home-server"
   homepage "https://github.com/pavele/homebrew-home-server"
-  url "https://github.com/pavele/homebrew-home-server/archive/refs/tags/v1.0.6.tar.gz"
+  url "https://github.com/pavele/homebrew-home-server/archive/refs/tags/v1.0.7.tar.gz"
   sha256 "d209731a23399c0fb03ce5322a7dd6981cd7ffdf14c97321ef1f045b107c3649"
   license "MIT"
-  version "1.0.6"
+  version "1.0.7"
   depends_on "nginx"
 
   def install
